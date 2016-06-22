8baa4382d25d566c9b1aa899704174782806fa1f
henriyli <henri.ylikotila@helsinki.fi>
2016-06-22 13:25:57 +0300

    updating the translation

--- yse-warning.js
+++ yse-warning.js
@@ -3,9 +3,9 @@ var YSE = YSE || {};
 
 YSE = {
     strings: {
-        'fi': 'Tätä käsite-ehdotusta ei ole vielä hyväksytty YSOon/YSAan. Jos tarvitset käsitettä kerro siitä käsite-ehdotuksen kotisivulla.',
-        'sv': 'Tätä käsite-ehdotusta ei ole vielä hyväksytty YSOon/YSAan. Jos tarvitset käsitettä kerro siitä käsite-ehdotuksen kotisivulla.',
-        'en': 'Tätä käsite-ehdotusta ei ole vielä hyväksytty YSOon/YSAan. Jos tarvitset käsitettä kerro siitä käsite-ehdotuksen kotisivulla.'
+        'fi': 'Tätä käsite-ehdotusta ei ole vielä hyväksytty YSAan eikä YSOon. Jos pidät käsitettä tarpeellisena tai haluat kommentoida sen sisältöä, kerro asiasta käsite-ehdotuksen kotisivulla (ks. linkki alla).',
+        'sv': 'Detta begreppsförslag har ännu inte godkänts till Allärs eller ALLFO. Om du anser att begreppet behövs eller om du vill kommentera det så berätta om det på begreppsförslagets sida (se länken nedan).',
+        'en': 'Tätä käsite-ehdotusta ei ole vielä hyväksytty YSAan eikä YSOon. Jos pidät käsitettä tarpeellisena tai haluat kommentoida sen sisältöä, kerro asiasta käsite-ehdotuksen kotisivulla (ks. linkki alla).'
     },
     renderWarning: function(params) {
         $('.content').prepend(Handlebars.compile($('#ysewarning-template').html())({'warning-text': this.strings[lang]}));
